8e0225bf7efabe78824a75e8eaa97150f98a4690
h-tanimoto <h-tanimoto@ruby-dev.jp>
2020-06-10 10:31:50 +0900

    create_chat_room

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,7 @@
 class Message < ApplicationRecord
   validates :content, presence: true
+  belongs_to :user
+  belongs_to :room
 
   after_create_commit { MessageBroadcastJob.perform_later self }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'rooms/show'
+  root 'rooms#index'
+  resources :rooms, only: %i[new show]
 end
